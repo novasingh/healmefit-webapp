ac0cca700e80ef6f4c5fe7db9f17c24c6395a684
HarshTCP1111 <harsh@techconciergepro.com>
2024-08-23 22:15:22 +0530

    document resolve

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -32,7 +32,7 @@ const initialDocuments = [
   },
   {
     id: 4,
-    name: "Vaccination proof",
+    name: "Vaccination Proof",
     type: "Vaccination Proof",
     status: "not_uploaded",
     description: "Need to have the booster",
@@ -130,6 +130,7 @@ function Documents() {
   const handleDelete = (document) => {
     if (document) {
 
+      console.log(document)
       remove(`/document/${userData?.id}/documents/${document._id}`).then((res) => {
         if(res){
           const updatedDocuments = documents.map((doc) =>
